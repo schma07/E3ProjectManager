daf74a026dcd6c7360864779853f126d873edf9c
schma <martinbraendle@yahoo.com>
2022-12-20 16:00:38 +0100

    Modified Dockerfile and started creating folder structure for upcoming entitities in Core

---
+++ Source/Core/Schma.E3ProjectManager.Core.Domain/Entities/ProjectAggregate/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schma.E3ProjectManager.Core.Domain.Entities.ProjectAggregate
+{
+    public class Project
+    {
+        public string description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+
+    }
+}
