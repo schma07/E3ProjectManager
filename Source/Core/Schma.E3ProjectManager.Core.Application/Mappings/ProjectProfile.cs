543a17550c65f4a1f19513b365edc3b5d927e741
schma <martinbraendle@yahoo.com>
2022-12-21 18:36:27 +0100

    - Created ProjectDeviceReadmodel

---
+++ Source/Core/Schma.E3ProjectManager.Core.Application/Mappings/ProjectProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Schma.E3ProjectManager.Core.Application.ReadModels;
+using Schma.E3ProjectManager.Core.Application.ReadModels.Orders;
+using Schma.E3ProjectManager.Core.Domain;
+using Schma.E3ProjectManager.Core.Domain.Entities.OrderAggregate;
+using Schma.E3ProjectManager.Core.Domain.Entities.ProjectAggregate;
+
+namespace Schma.E3ProjectManager.Core.Application.Mappings
+{
+    internal class ProjectProfile : Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<Project, ProjectReadModel>();
+            CreateMap<ProjectDevice, ProjectDeviceReadModel>();            
+        }
+    }
+}
---
+++ Source/Core/Schma.E3ProjectManager.Core.Application/ReadModels/Projects/ProjectDeviceReadModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Schma.E3ProjectManager.Core.Application.ReadModels.Projects
+{
+    public class ProjectDeviceReadModel
+    {
+        public string SupplierArticleNumber { get; set; }
+        public string DeviceName { get; set; }
+        public string DeviceLocation { get; set; }
+        public string DeviceFunction { get; set; }
+        public int Quantity { get; set; }
+    }
+}
