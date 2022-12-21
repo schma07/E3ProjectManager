4253e7eaf2f93f54dd756a36688f9e378330a4fe
schma <martinbraendle@yahoo.com>
2022-12-21 18:41:07 +0100

    - Created ProjectReadModel

---
+++ Source/Core/Schma.E3ProjectManager.Core.Application/ReadModels/Projects/ProjectReadModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Schma.E3ProjectManager.Core.Application.ReadModels.Projects
+{
+    public class ProjectReadModel
+    {
+        public Guid Id { get; set; }
+        public string TrackingNumber { get; set; }
+        public string ProjectName { get; set; }
+        public List<ProjectDeviceReadModel> ProjectDevices { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
